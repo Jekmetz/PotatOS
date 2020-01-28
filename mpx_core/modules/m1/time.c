3b940f13d876f7128128899356e81a17ba358e4e
henry <henry@oem-VirtualBox>
2020-01-28 00:07:32 -0500

    Time is able to be retrieved

--- mpx_core/modules/m1/time.c
+++ mpx_core/modules/m1/time.c
@@ -1,81 +1,154 @@
 #include <string.h>
 #include <core/io.h>
+#include <core/utility.h>
+#include <core/serial.h>
 
 #include "time.h"
 
-#define cat_val(scale) \
+#define pull_data(val, loc) \
 {\
-	strcat(dest, ":");\
-	itoa(time->scale, buff, 10);\
-	strcat(dest, buff);\
+	outb(INDEX_REG, loc);\
+	char temp = inb(DATA_REG);\
+	val = bcd_to_decimal(temp);\
+	if (val < 0)\
+	{\
+		serial_println("\033[31mTIME ERROR: BCD conversion error in "#val".\033[0m");\
+		return (time_h){-1,-1,-1,-1,-1,-1};\
+	}\
 }
 
-#define dow_cases(abbr) \
+#define decimal_to_bcd(val) ((val/10)<<4 | (val%10))
+
+#define neg_safe_set(in, loc) \
 {\
-	strcpy(buff, abbr);\
-	break;\
+	if (in > -1)\
+	{\
+		outb(INDEX_REG, loc);\
+		outb(DATA_REG, decimal_to_bcd(in));\
+	}\
+}
+
+int bcd_to_decimal(int bcd){
+	// Accepts a bcd value as bcd
+	// We need to take the first nyble and multiply by 10
+	// Then add the second nyble
+
+	int ret;
+	ret = ((bcd & 0xF0) >> 4) * 10 + (bcd & 0x0F);
+	return ret;
 }
 
 void format_time(char *dest, time_h *time)
 {
-	char buff[5];
-	itoa(time->year, buff, 10);
-	strcpy(dest, buff);
-
-	cat_val(month);
-	cat_val(day_of_month);
-	strcat(dest, "/");
-	switch (time->day_of_week)
-	{
-		case SUNDAY:
-		dow_cases("SUN");
-		case MONDAY:
-		dow_cases("MON");
-		case TUESDAY:
-		dow_cases("TUE");
-		case WEDNESDAY:
-		dow_cases("WED");
-		case THURSDAY:
-		dow_cases("THU");
-		case FRIDAY:
-		dow_cases("FRI");
-		case SATURDAY:
-		dow_cases("SAT");
-		default:
-		dow_cases("ERR");
-	}
-	strcat(dest, buff);
-
-	cat_val(hours);
-	cat_val(minutes);
-	cat_val(seconds);
-
-	strcat(dest, " 1.3");
+	sprintf(dest, "%d:%d:%d:%d:%d:%d", time->year, time->month, time->day_of_month, time->hours, time->minutes, time->seconds);
 }
 
 time_h get_current_time()
 {
-	char dat = inb(ACCESS_REG);
-	dat |= 0x80;
-	outb(ACCESS_REG, dat);
-
 	time_h ret;
-	ret.seconds = ((int)inb(SECOND_REG));
-	ret.minutes = ((int)inb(MINUTE_REG));
-	ret.hours = ((int)inb(HOUR_REG));
-	ret.day_of_week = ((int)inb(DAY_OF_WEEK_REG));
-	ret.day_of_month = ((int)inb(DAY_OF_MONTH_REG));
-	ret.month = ((int)inb(MONTH_REG));
-	ret.year = ((int)inb(YEAR_REG));
+	pull_data(ret.day_of_month, DAY_OF_MONTH_REG);
+	pull_data(ret.month, MONTH_REG);
+	int century, year;
+	pull_data(century, CENTURY_REG);
+	pull_data(year, YEAR_REG);
+	ret.year = century * 100 + year;
 
-	dat = inb(ACCESS_REG);
-	dat &= 0x7F;
-	outb(ACCESS_REG, dat);
+	pull_data(ret.hours, HOUR_REG);
+	pull_data(ret.minutes, MINUTE_REG);
+	pull_data(ret.seconds, SECOND_REG);
 
 	return ret;
 }
 
-//void set_current_time(time_h time)
-//{
+int set_current_time(time_h time)
+{
+	time_h curr = get_current_time();
 
-//}
+	// checking year validity
+	// also parsing into century and year
+	int century, year;
+	if (time.year > -1)
+	{
+		if (time.year < 1700 || time.year > 3000)
+		{
+			char msg[64];
+			sprintf(msg, "\033[31mTIME ERROR: The year %d is not valid.\033[0m", time.year);
+			serial_println(msg);
+			return 0;
+		}
+		century = time.year / 100;
+		year = time.year % 100;
+
+		curr.year = time.year;
+	}
+	else
+	{
+		century = -1;
+		year = -1;
+	}
+
+	// checking month validity
+	if (time.month > -1)
+	{
+		if (time.month == 0 || time.month > 12)
+		{
+			char msg[64];
+			sprintf(msg, "\033[31mTIME ERROR: The month %d is not valid.\033[0m", time.month);
+			serial_println(msg);
+			return 0;
+		}
+		curr.month = time.month;
+	}
+
+	// checking if day is valid
+	if (time.day_of_month > -1)
+	{
+		if (time.day_of_month == 0 || // day cannot be 0 (negatives are allowed because of no write case)
+		(curr.month == 2 && curr.year % 4 == 0 && time.day_of_month > 29) || // leap year in FEBRUARY
+		(curr.month == 2 && time.day_of_month > 28) || // FEBRUARY
+		(curr.month % 2 == 0 && time.day_of_month > 30) || // Even months have 30 days
+		(time.day_of_month > 31)) // no month has more than 31 days
+		{
+			char msg[64];
+			sprintf(msg, "\033[31mTIME ERROR: The day %d is not valid.\033[0m", time.day_of_month);
+			serial_println(msg);
+			return 0;
+		}
+		curr.day_of_month = time.day_of_month;
+	}
+
+	if (time.hours > 24)
+	{
+		char msg[64];
+		sprintf(msg, "\033[31mTIME ERROR: The hour %d is not valid.\033[0m", time.hours);
+		serial_println(msg);
+		return 0;
+	}
+
+	if (time.minutes > 59)
+	{
+		char msg[64];
+		sprintf(msg, "\033[31mTIME ERROR: The minute %d is not valid.\033[0m", time.minutes);
+		serial_println(msg);
+		return 0;
+	}
+
+	if (time.seconds > 59)
+	{
+		char msg[64];
+		sprintf(msg, "\033[31mTIME ERROR: The second %d is not valid.\033[0m", time.seconds);
+		serial_println(msg);
+		return 0;
+	}
+
+	sti();
+	neg_safe_set(time.seconds, SECOND_REG);
+	neg_safe_set(time.minutes, MINUTE_REG);
+	neg_safe_set(time.hours, HOUR_REG);
+	neg_safe_set(century, CENTURY_REG);
+	neg_safe_set(year, YEAR_REG);
+	neg_safe_set(time.month, MONTH_REG);
+	neg_safe_set(time.day_of_month, DAY_OF_MONTH_REG);
+	cli();
+	return 1; // Successful exit
+}
--- mpx_core/modules/m1/time.h
+++ mpx_core/modules/m1/time.h
@@ -1,51 +1,39 @@
 #pragma once
 
-#define SECOND_REG 0x71
-#define MINUTE_REG 0x72
-#define HOUR_REG 0x73
-#define DAY_OF_WEEK_REG 0x74
-#define DAY_OF_MONTH_REG 0x75
-#define MONTH_REG 0x76
-#define YEAR_REG 0x77
-#define ACCESS_REG 0x70
-
-
-enum DAY_OF_WEEK
-{
-    SUNDAY,
-    MONDAY,
-    TUESDAY,
-    WEDNESDAY,
-    THURSDAY,
-    FRIDAY,
-    SATURDAY
-};
+#define SECOND_REG 0x00
+#define MINUTE_REG 0x02
+#define HOUR_REG 0x04
+#define DAY_OF_MONTH_REG 0x07
+#define MONTH_REG 0x08
+#define CENTURY_REG 0x32
+#define YEAR_REG 0x09
+#define INDEX_REG 0x70
+#define DATA_REG 0x71
 
 enum MONTH
 {
-    JANUARY = 1,
-    FEBRUARY,
-    MARCH,
-    APRIL,
-    MAY,
-    JUNE,
-    JULY,
-    AGUST,
-    SEPTEMBER,
-    OCTOBER,
-    NOVEMBER,
-    DECEMBER
+  JANUARY = 1,
+  FEBRUARY,
+  MARCH,
+  APRIL,
+  MAY,
+  JUNE,
+  JULY,
+  AGUST,
+  SEPTEMBER,
+  OCTOBER,
+  NOVEMBER,
+  DECEMBER
 };
 
 struct time
 {
-    int seconds;
-    int minutes;
-    int hours;
-    int day_of_week;
-    int day_of_month;
-    int month;
-    int year;
+  int seconds;
+  int minutes;
+  int hours;
+  int day_of_month;
+  int month;
+  int year;
 };
 typedef struct time time_h;
 
@@ -53,4 +41,4 @@ void format_time(char *dest, time_h *t);
 
 time_h get_current_time();
 
-//void set_current_time(time_h time);
+int set_current_time(time_h time);
