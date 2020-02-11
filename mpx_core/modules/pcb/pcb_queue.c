f6fe43fb7d43e379c7efcd20bff44798475fbf33
tntman42 <tntman42@gmail.com>
2020-02-11 15:31:18 -0500

    Added comments, and fixed the kill process function

--- mpx_core/modules/pcb/pcb_queue.c
+++ mpx_core/modules/pcb/pcb_queue.c
@@ -1,50 +1,103 @@
 #include "pcb_queue.h"
 #include "../mpx_supt.h"
 
+#include <string.h>
+
+// definitions of success and failure for true/false returns
+#define SUCCESS 1
+#define FAILURE 0
+
 void enqueue(queue_t *que, pcb_t *data)
 {
+    // allocating node space
     struct node *nnode = sys_alloc_mem(sizeof(struct node));
+
+    // setting node data
     nnode->data = data;
     nnode->prev = que->size > 0 ? que->tail : NULL;
     nnode->next = NULL;
+
+    // appending new node to tail
     que->tail = nnode;
+
+    // checking for edge case of no data in queue
     if (que->size == 0)
     {
         que->head = nnode;
     }
+    // incrementing size
     que->size++;
 }
 
 pcb_t *dequeue(queue_t *queue)
 {
+    // getting head
     pcb_t *ret = queue->head->data;
+    // saving head in a local pointer
     struct node *ded = queue->head;
+    // shifting head
     queue->head = queue->head->next;
+    // removing reference to old head
     queue->head->prev = NULL;
-    sys_free_mem(ded);
-    queue->size--;
-    return ret;
-}
 
-pcb_t *peek(queue_t *queue)
-{
-    return queue->head->data;
+    // freeing old head
+    sys_free_mem(ded);
+    // decrementing queue
+    queue->size--;
+
+    // returning dequeued data
+    return ret;
 }
 
 queue_t *construct_queue()
 {
+    // allocating queue
     queue_t *que = sys_alloc_mem(sizeof(queue_t));
+    // setting data
     que->head = NULL;
     que->tail = NULL;
     que->size = 0;
+    // returning pointer
     return que;
 }
 
 void destruct_queue(queue_t *queue)
 {
+    // checking if the que has any contents
+    struct node *curr = queue->head;
     while (queue->size > 0)
     {
-        dequeue(queue);
+        // destroying data
+        curr = curr->next;
+        sys_free_mem(((struct node *)curr->prev)->data);
+        sys_free_mem(curr->prev);
+        queue->size--;
     }
+    // freeing queue
     sys_free_mem(queue);
+}
+
+/**
+ *  FIXME: This is not merciless enough
+ */
+int remove_pcb(queue_t *que, char *name)
+{
+    // setting the moving pointer to start at head
+    struct node *curr = que->head;
+    while (curr != NULL)
+    {
+        // checking if the process is the correct one
+        if (strcmp(curr->data->process_name, name) == 0)
+        {
+            // destroying the process
+            ((struct node *)curr->prev)->next = curr->next;
+            ((struct node *)curr->next)->prev = curr->prev;
+            sys_free_mem(curr->data);
+            sys_free_mem(curr);
+            que->size--;
+            return SUCCESS;
+        }
+        curr = curr->next;
+    }
+    return FAILURE;
 }
--- mpx_core/modules/pcb/pcb_queue.h
+++ mpx_core/modules/pcb/pcb_queue.h
@@ -1,11 +1,19 @@
 #pragma once
 
+/**
+ *  Contains all possible process classes
+ * 
+ *  TODO: there may be more
+ */
 enum PROCESS_CLASS
 {
     SYSTEM,
     APPLICATION
 };
 
+/**
+ *  Contains all possible process states
+ */
 enum PROCESS_STATE
 {
     READY,
@@ -13,13 +21,9 @@ enum PROCESS_STATE
     BLOCKED
 };
 
-enum SUCCESS_STATES
-{
-    FAILED,
-    SUCCESS,
-    NOT_FOUND
-};
-
+/**
+ *  Struct that contains all information related to a pcb
+ */
 struct pcb
 {
     char *process_name;
@@ -30,6 +34,11 @@ struct pcb
 };
 typedef struct pcb pcb_t;
 
+/**
+ *  One element within the pcb queue
+ * 
+ *  This allows us to abbreviate code elsewhere... probably
+ */
 struct node
 {
     pcb_t *data;
@@ -38,6 +47,9 @@ struct node
     void *prev;
 };
 
+/**
+ *  Contains all the data needed to use/modify a queue
+ */
 struct queue
 {
     int size;
@@ -46,14 +58,40 @@ struct queue
 };
 typedef struct queue queue_t;
 
+/**
+ *  Appends an element onto the tail of the given queue
+ * 
+ *  This also allocates memory for a node
+ *  Note that the data should also be allocated dynamically
+ */
 void enqueue(queue_t *que, pcb_t *data);
 
+/** 
+ *  Takes the PCB off of the head of the queue and moves head
+ * 
+ *  Takes care of freeing the node
+ *  returns the head PCB
+ */
 pcb_t *dequeue(queue_t *queue);
 
-pcb_t *peek(queue_t *queue);
-
+/**
+ *  Creates a queue
+ * 
+ *  Creates a queue and sets all variables correctly for initialization
+ */
 queue_t *construct_queue();
 
+/**
+ *  Destructs a queue
+ * 
+ *  De-allocates a queue and all of the elements within it.
+ */
 void destruct_queue(queue_t *);
 
-int kill_process(char *name);
+/**
+ *  Attempts to find a process by name, and destroys it
+ * 
+ *  If this process cannot find the process, it will fail (return false/0)
+ *  if the process is successfully immolated, it will succeed (return true/1)
+ */
+int remove_pcb(queue_t *queue, char *name);
