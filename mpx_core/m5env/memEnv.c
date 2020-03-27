f36f065f9e0b7917ef2cd4ed6a07ddc0781dce72
jek0025 <jek0025@mix.wvu.edu>
2020-03-27 01:32:56 -0400

    Get a half decent testing environment up there. Workinggit status YAYgit status

--- mpx_core/m5env/memEnv.c
+++ mpx_core/m5env/memEnv.c
@@ -1,83 +1,98 @@
-#include "memory_wrangler.h"
-#include "../mpx_supt.h"
-#include <system.h>
-#include <mem/heap.h>
+#include "memEnv.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-void* fma;
+void* sma;
 cmcb* ffree;
 cmcb* falive;
 
 unsigned int remaining_free;
 
+typedef unsigned char* location;
+
 #define SUCCESS 1
 #define FAILURE 0
 
-#define DEBUG 1
+#define DEBUG 0
 
-#if DEBUG
-#include <core/stdio.h>
 #define PRINT(str, dat...) \
   printf("\033[35mMEM_WRANGLER(%d):\033[0m " str "\033[0m\n", __LINE__, dat)
-#endif
 
 #define NEXT_BLOCK(block) \
   (cmcb*)((unsigned char*)block + block->size + MCB_PADDING)
+
 #define GET_LMCB(block) \
   (lmcb*)((unsigned char*)block + block->size + sizeof(cmcb))
+
 #define ASCMCB(loc) \
   ((cmcb*)loc)
+
 #define ASLMCB(loc) \
   ((lmcb*)loc)
 
-typedef unsigned char* location;
+#define NEXT_BLOCK(block) \
+  (cmcb*)((unsigned char*)block + block->size + MCB_PADDING)
 
-void* mem_init() {
-  // Creating heap
-  fma = (void*)kmalloc(HEAP_SIZE + MCB_PADDING);
+int main()
+{
+	mem_init();
 
-  // Setting free block
-  ffree = fma;
-  falive = NULL;
-  ffree->karen = NULL;
-  ffree->size = HEAP_SIZE;
-  ffree->type = FREE;
+	void* a = internal_malloc(10);
+	void* b = internal_malloc(48);
+	void* c = internal_malloc(50);
+	void* d = internal_malloc(20);
+	void* e = internal_malloc(9);
 
-  // ending block
-  lmcb* end = (lmcb*)((location)fma + sizeof(cmcb) + HEAP_SIZE);
-  end->top = ffree;
-  end->type = FREE;
+	internal_free(a);
+	internal_free(b);
+	internal_free(c);
+    internal_free(d);
+	internal_free(e);
 
-  remaining_free = HEAP_SIZE;
+	show_mem_state();
 
-#if DEBUG
-  PRINT("Created heap with \033[32;1m%d bytes", HEAP_SIZE + MCB_PADDING);
-#endif
-  return fma;
+	return 0;
 }
 
-void* internal_malloc(u32int size) {
-  pcb_t* karen = (pcb_t*)get_running_process();
-#if DEBUG
-  if (karen != NULL) {
-    PRINT("Allocating memory for %s - %d", karen->process_name, size);
-  }
-  else {
-    PRINT("Allocating memory for NULL - %d bytes", size);
-  }
-#endif
+void* mem_init()
+{
+	//creating heap
+	sma = (void*)malloc(HEAP_SIZE + MCB_PADDING);
+
+	//Setting free block
+	ffree = sma;
+	falive = NULL;
+	ffree->karen = NULL;
+	ffree->size = HEAP_SIZE;
+	ffree->type = FREE;
+
+	//ending block
+	lmcb* end = (lmcb*)((location)sma + sizeof(cmcb) + HEAP_SIZE);
+	end->top = ffree;
+	end->type = FREE;
+
+	remaining_free = HEAP_SIZE;
+
+	PRINT("Created heap with \033[32;1m%ld bytes", HEAP_SIZE + MCB_PADDING);
+	return sma;
+}
+
+void* internal_malloc(u32int size)
+{
+	pcb_t* karen = (pcb_t*)malloc(sizeof(pcb_t));
+	karen->process_name = "heyo!";
+
   // checking that there is enough memory in the heap
-  if (remaining_free < (size + MCB_PADDING)) {
+  if (remaining_free < size + MCB_PADDING) {
     return NULL;
   }
 
-  // finding fisrt available free block
-
+  // finding first available free block
   cmcb* curr_blk = ffree;
   while ((curr_blk->size < size || curr_blk->type == ALIVE) &&
-         (location)curr_blk < (location)fma + HEAP_SIZE + MCB_PADDING) {
+         (location)curr_blk < ((location)sma + HEAP_SIZE + MCB_PADDING)) {
     curr_blk = NEXT_BLOCK(curr_blk);
-    if ((location)curr_blk > (location)fma + HEAP_SIZE + sizeof(cmcb)) {
-      //If we have run out of spaces...
+    if ((location)curr_blk > (location)sma + HEAP_SIZE + sizeof(cmcb)) {
       return NULL;
       //curr_blk = ffree;
     }
@@ -91,21 +106,18 @@ void* internal_malloc(u32int size) {
 
   if( falive == NULL || (location)curr_blk < (location)falive)
   {
-    falive = curr_blk;
+  	falive = curr_blk;
   }
 
-  //If there is not enough space to fit another block padding and an int...
   if( (prev_size - size) < MCB_PADDING + sizeof(int))
   {
-    lmcb* e_blk = (lmcb*)((location)curr_blk + prev_size + sizeof(cmcb));
-    curr_blk->size = prev_size;
-    e_blk->top = curr_blk;
-    e_blk->type = ALIVE;
-    remaining_free -= prev_size;
-    return (void*)((location)curr_blk + sizeof(cmcb));
+  	lmcb* e_blk = (lmcb*)((location)curr_blk + prev_size + sizeof(cmcb));
+  	curr_blk->size = prev_size;
+  	e_blk->top = curr_blk;
+  	e_blk->type = ALIVE;
+  	remaining_free -= prev_size;
+  	return (void*)((location)curr_blk + sizeof(cmcb));
   }
-
-  //If there is enough space to fit another Free block...
 
   lmcb* e_blk = (lmcb*)((location)curr_blk + size + sizeof(cmcb));
   e_blk->top = curr_blk;
@@ -133,7 +145,8 @@ void* internal_malloc(u32int size) {
   return (void*)((location)curr_blk + sizeof(cmcb));
 }
 
-int internal_free(void* data) {
+int internal_free(void* data)
+{
   cmcb* block = data - sizeof(cmcb);
   lmcb* end = data + block->size;
 
@@ -146,7 +159,7 @@ int internal_free(void* data) {
   print_both(block,end,"JUST BEFORE FREEING");
   #endif
 
-    // Setting variables of current block
+  // Setting variables of current block
   block->type = FREE;
   end->type = FREE;
   block->karen = NULL;
@@ -154,7 +167,7 @@ int internal_free(void* data) {
 
   // checking previous block
   lmcb* prev_lmcb = (lmcb*)((location)block - sizeof(lmcb));
-  if ((location)prev_lmcb >= (location)fma && prev_lmcb->type == FREE) {
+  if ((location)prev_lmcb >= (location)sma && prev_lmcb->type == FREE) {
     prev_lmcb->top->size += block->size + MCB_PADDING;
     end->top = prev_lmcb->top;
     block = end->top;
@@ -163,7 +176,7 @@ int internal_free(void* data) {
 
   // checking next block
   cmcb* next_cmcb = (cmcb*)((location)block + MCB_PADDING + block->size);
-  if ((location)next_cmcb < ((location)fma + HEAP_SIZE + MCB_PADDING) &&
+  if ((location)next_cmcb < ((location)sma + HEAP_SIZE + MCB_PADDING) &&
       next_cmcb->type == FREE) {
     lmcb* next_lmcb = GET_LMCB(next_cmcb);
     next_lmcb->top = block;
@@ -171,7 +184,7 @@ int internal_free(void* data) {
     remaining_free += MCB_PADDING;
   }
 
-  if (block == fma) {
+  if (block == sma) {
     ffree = block;
     falive = NEXT_BLOCK(block);
   }
@@ -182,10 +195,10 @@ int internal_free(void* data) {
 void print_cmcb(cmcb* p)
 {
   printf(
-    "CMCB INFO: (%d BYTES)\n"
+    "CMCB INFO: (%li BYTES)\n"
     "karen: %s\n"
-    "cmcbloc: %x\n"
-    "datloc: %x\n"
+    "cmcbloc: %p\n"
+    "datloc: %p\n"
     "size: %d\n"
     "type: %s\n\n",
     sizeof(cmcb),
@@ -200,9 +213,9 @@ void print_cmcb(cmcb* p)
 void print_lmcb(lmcb* p)
 {
     printf(
-    "LMCB INFO: (%d BYTES)\n"
-    "lmcbloc: %x\n"
-    "top: %x\n"
+    "LMCB INFO: (%li BYTES)\n"
+    "lmcbloc: %p\n"
+    "top: %p\n"
     "type: %s\n\n",
     sizeof(lmcb),
     p,
@@ -220,17 +233,17 @@ void print_both(cmcb* c, lmcb* l, char* msg)
 
 void show_mem_state()
 {
-  void *curr_blk = fma;
-  unsigned int covered = 0;
+  void *curr_blk = sma;
+  int covered = 0;
 
   while(covered < (HEAP_SIZE + MCB_PADDING))
   {
-    covered += ASCMCB(curr_blk)->size + MCB_PADDING;
-    printf("%s : %x\n",ASCMCB(curr_blk)->type == FREE ? "FCMCB" : "ACMCB", curr_blk);
-    printf("%d\n",ASCMCB(curr_blk)->size);
-    curr_blk += sizeof(cmcb) + ASCMCB(curr_blk)->size;
-    printf("%s\n",ASLMCB(curr_blk)->type == FREE ? "FLMCB" : "ALMCB");
-    curr_blk += sizeof(lmcb);
+  	covered += ASCMCB(curr_blk)->size + MCB_PADDING;
+  	printf("%s : %p\n",ASCMCB(curr_blk)->type == FREE ? "FCMCB" : "ACMCB", curr_blk);
+  	printf("%d\n",ASCMCB(curr_blk)->size);
+  	curr_blk += sizeof(cmcb) + ASCMCB(curr_blk)->size;
+  	printf("%s\n",ASLMCB(curr_blk)->type == FREE ? "FLMCB" : "ALMCB");
+  	curr_blk += sizeof(lmcb);
   }
 
   puts("END OF HEAP");
--- mpx_core/m5env/memEnv.h
+++ mpx_core/m5env/memEnv.h
@@ -1,19 +1,34 @@
-#include "../pcb/pcb_constants.h"
-
-#define HEAP_SIZE 54301
+#define HEAP_SIZE 300
 #define MCB_PADDING (sizeof(cmcb) + sizeof(lmcb))
+
+//Compat
+typedef unsigned int u32int;
 
 typedef enum { FREE, ALIVE } MEMTYPE;
 
+/**
+ *  @brief Struct that contains all information related to a pcb
+ */
+typedef struct pcb
+{
+    char *process_name;
+    u32int process_class;
+    u32int priority;
+    u32int last_time_run;
+    u32int state;
+    char stack[2048];
+    unsigned char *stacktop;
+} pcb_t;
+
 typedef struct {
-  pcb_t* karen;
-  unsigned int size;
-  MEMTYPE type;
+	pcb_t* karen;
+	unsigned int size;
+	MEMTYPE type;
 } cmcb;
 
 typedef struct {
-  cmcb* top;
-  MEMTYPE type;
+	cmcb* top;
+	MEMTYPE type;
 } lmcb;
 
 void* mem_init();
